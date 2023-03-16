56d1ae522b9f9f5b45030d0d41c0408fe9958722
ALcitya <ichwanalif55@gmail.com>
2023-03-16 11:04:37 +0700

    Merubah isi alert di fungsi dan mengubah isi case

--- kalkulator/src/js/app.js
+++ kalkulator/src/js/app.js
@@ -1,23 +1,23 @@
 let condition
 const tambah = function (a, b){
     let hasilTambah = a + b
-    alert(hasilTambah)
-    condition = confirm("ulang?")
+    alert(`Hasil dari ${a} + ${b} adalah ${hasilTambah}`)
+    condition=confirm("Ulangi dari awal?")
 }
 const kurang = function (a, b){
     let hasilKurang = a - b
-    alert(hasilKurang)
-    condition = confirm("ulang")
+    alert(`Hasil dari ${a} - ${b} adalah ${hasilKurang}`)
+    condition=confirm("Ulangi dari awal?")
 }
 const kali = function (a ,b){
     let hasilKali = a*b
-    alert(hasilKali)
-    condition = confirm("ulang")
+    alert(`Hasil dari ${a} * ${b} adalah ${hasilKali}`);
+    condition=confirm("Ulangi dari awal?")
 }
 const bagi = function (a,b){
     let hasilBagi = a/b
-    alert(hasilBagi)
-    condition = confirm("ulang?")
+    alert(`Hasil dari ${a} / ${b} adalah  ${hasilBagi}`)
+    condition=confirm("Ulangi dari awal?")
 }
 
 const program = function (pilihanPerhitungan){
@@ -25,23 +25,23 @@ const program = function (pilihanPerhitungan){
     input2=parseInt(prompt("Nilai 2"))
     switch(parseInt(pilihanPerhitungan)){
         case 1:
-            alert(`Hasil dari ${input1} + ${input2} adalah ${tambah(input1, input2)}`);
+            tambah(input1,input2);
             break
         case 2:
-            alert(`Hasil dari ${input1} - ${input2} adalah ${kurang(input1, input2)}`);
+            kurang(input1,input2);
             break
         case 3:
-            alert(`Hasil dari ${input1} * ${input2} adalah ${kali(input1,input2)}`);
+            kali(input1,input2);
             break
         case 4:
-            alert(`Hasil dari ${input1} / ${input2} adalah  ${bagi(input1,input2)}`)
+            bagi(input1,input2);
             break
         default:
             alert("Pilihan perhitungan belum tersedia")
     }
 }
-let pilihanPerhitungan=prompt("Masukkan pilihan perhitungan")
 do{
+let pilihanPerhitungan=prompt("Masukkan pilihan perhitungan")
     program(pilihanPerhitungan)
 }while(condition)
 
