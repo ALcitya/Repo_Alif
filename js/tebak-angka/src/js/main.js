10eac09f117b676f82a597b54b62405a0acde6ef
ALcitya <ichwanalif55@gmail.com>
2023-03-22 23:09:00 +0700

    Membuat game tebak angka v2

--- js/tebak-angka/src/js/main.js
+++ js/tebak-angka/src/js/main.js
@@ -1,24 +1,25 @@
 function random(){
     const number = [0,1,2,3,4,5,6,7,8,9]
-    number.sort(function(){
+    let hasil= number.sort(function(){
         return Math.random() - 0.5
     })
-   let index_random = Math.floor(Math.random() * number.length - 1)
-   return number[index_random]
+    let index_random = Math.floor(Math.random() * number.length - 1)
+    return(number[index_random])
 }
+
 function input(){
     let nilai = prompt("Masukkan Angka yang kamu tebak")
     return parseInt(nilai)
 }
 const game = function(user_input, comp_input){
     if(user_input === comp_input){
-        alert("Selamat anda benar")
+        alert(`Selamat Anda benar, angka yang anda tebak adalah ${user_input} \n dan angka komputer adalah ${comp_input}`)
     }
     else{
-        alert("maaf anda salah")
+        alert(`Maaf anda salah, angka yang anda tebak adalah ${user_input} \n dan angka komputer adalah ${comp_input}`)
     }
 }
+
 let comp = random()
-console.log(comp)
 let user = input()
 game(user , comp)
